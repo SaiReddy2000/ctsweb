acabea9e2c8814e43bb5c55423ecf590486a8694
Sai <saihru16@gmail.com>
2022-10-28 14:59:06 +0400

    Commit 1

---
+++ src/main/java/com/sai/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.sai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+	
+	@GetMapping
+	@RequestMapping("{name}")
+	public String getName(@PathVariable String name) {
+		return "Hi " + name;
+	}
+
+}
